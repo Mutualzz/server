df4a1ba4cb277e3a5d4c6f0078fe6877c1477d7c
mateie <mateie@gmail.com>
2025-09-01 22:13:40 -0700

    Constants: Updated max file size 16

--- src/rest/utils/Constants.ts
+++ src/rest/utils/Constants.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_PORT = 3000;
-export const MAX_FILE_SIZE_MB = 8;
+export const MAX_FILE_SIZE_MB = 16;
 export const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024;
 
 export const BCRYPT_SALT_ROUNDS = 11;
