29e0d980f4b354d92b0ffe524e3310ddea6db72b
mateie <mateie@gmail.com>
2025-09-05 18:41:27 -0700

    Fixed some tsdown stuff

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-    entry: ["src/**/**/*"],
+    entry: ["./src/**/*"],
     clean: true,
     format: "esm",
     target: "esnext",
