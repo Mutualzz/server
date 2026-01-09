d658b64fe2c2cbde8c7fbfc67d76d6737577b6e8
Azrael <azrael@mutualzz.com>
2026-01-09 00:51:20 -0800

    test

--- src/util/RabbitMQ.ts
+++ src/util/RabbitMQ.ts
@@ -15,7 +15,7 @@ export class RabbitMQ {
     static async init() {
         this.connection = await amqplib.connect(
             {
-                hostname: process.env.RABBIT_HOST || "localhost",
+                hostname: process.env.RABBIT_HOST || "rabbitmq",
                 username: process.env.RABBIT_USERNAME,
                 password: process.env.RABBIT_PASSWORD,
             },
