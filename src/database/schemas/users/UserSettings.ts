5a1cb90e5fa6394fb5d3333ae75a102709bdb122
Azrael <azrael@mutualzz.com>
2026-06-26 00:50:58 -0700

    new default

--- src/database/schemas/users/UserSettings.ts
+++ src/database/schemas/users/UserSettings.ts
@@ -1,40 +1,40 @@
 import {
-    bigint,
-    boolean,
-    pgEnum,
-    pgTable,
-    text,
-    timestamp,
+  bigint,
+  boolean,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
 } from "drizzle-orm/pg-core";
 import { usersTable } from "./User";
 
 export const preferredModeEnum = pgEnum("preferred_mode", ["spaces", "feed"]);
 
 export const userSettingsTable = pgTable("user_settings", {
-    userId: bigint({ mode: "bigint" })
-        .primaryKey()
-        .references(() => usersTable.id, {
-            onDelete: "cascade",
-            onUpdate: "cascade",
-        }),
+  userId: bigint({ mode: "bigint" })
+    .primaryKey()
+    .references(() => usersTable.id, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
 
-    currentTheme: text(),
-    currentIcon: text(),
+  currentTheme: text().default("baseDark"),
+  currentIcon: text(),
 
-    preferredMode: preferredModeEnum().default("spaces").notNull(),
-    preferEmbossed: boolean().notNull().default(false),
+  preferredMode: preferredModeEnum().default("spaces").notNull(),
+  preferEmbossed: boolean().notNull().default(false),
 
-    preferredSelfMute: boolean().notNull().default(false),
-    preferredSelfDeaf: boolean().notNull().default(false),
+  preferredSelfMute: boolean().notNull().default(false),
+  preferredSelfDeaf: boolean().notNull().default(false),
 
-    spacePositions: bigint({ mode: "bigint" }).array().default([]).notNull(),
+  spacePositions: bigint({ mode: "bigint" }).array().default([]).notNull(),
 
-    favoriteEmojis: text().array().default([]).notNull(),
-    favoriteGifs: text().array().default([]).notNull(),
-    favoriteStickers: text().array().default([]).notNull(),
+  favoriteEmojis: text().array().default([]).notNull(),
+  favoriteGifs: text().array().default([]).notNull(),
+  favoriteStickers: text().array().default([]).notNull(),
 
-    updatedAt: timestamp()
-        .notNull()
-        .defaultNow()
-        .$onUpdate(() => new Date()),
+  updatedAt: timestamp()
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
 });
